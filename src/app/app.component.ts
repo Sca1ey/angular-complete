08f3c22ba89f1f94a88ca590a57a8e14731c3a74
Sca1ey <tim@thurgoland.net>
2021-04-28 22:05:39 +0100

    assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //selector: '[app-root]' <div app-root><div/>
+  //selector: '.app-root'  <div class="app-root"><div/>
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
   styles: [
