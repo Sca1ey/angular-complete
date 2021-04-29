0c7532018af93f397fa1719bc09f45a34c68fc4f
Sca1ey <tim@thurgoland.net>
2021-04-29 21:13:01 +0100

    completed assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  //selector: '[app-root]' <div app-root><div/>
-  //selector: '.app-root'  <div class="app-root"><div/>
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-complete';
+  username = '';
+
+  resetOnClick() {
+    this.username = '';
+  }
 }
