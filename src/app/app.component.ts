9aca4268121e5634e2a00dde888e5746eb7bccee
Sca1ey <tim@thurgoland.net>
2021-04-30 22:14:12 +0100

    assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  //selector: '[app-root]' <div app-root><div/>
-  //selector: '.app-root'  <div class="app-root"><div/>
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-complete';
+  hide = false;
+  clicks = [];
+  toggle(): void {
+    this.clicks.push(this.clicks.length + 1);
+    this.hide === true ? (this.hide = false) : (this.hide = true);
+  }
 }
