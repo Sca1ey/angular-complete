e156253afdc4efc36aa1d193cab4b305ebe77d55
Sca1ey <tim@thurgoland.net>
2021-05-01 17:26:05 +0100

    ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Pizza',
+      'Try our delicious pizza with a wide range of topping options.',
+      'https://images.squaremeal.co.uk/cloud/article/9339/images/pizza-express-favourite-casual-dining-brand_18092019094334.jpg'
+    ),
+    new Recipe(
+      'Pizza',
+      'Try our delicious pizza with a wide range of topping options.',
+      'https://images.squaremeal.co.uk/cloud/article/9339/images/pizza-express-favourite-casual-dining-brand_18092019094334.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
