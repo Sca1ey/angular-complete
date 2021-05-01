dac9b0044e7d5ca2addc24d88c7103f1c84b77e6
Sca1ey <tim@thurgoland.net>
2021-05-01 12:38:08 +0100

    setup empty project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  //selector: '[app-root]' <div app-root><div/>
-  //selector: '.app-root'  <div class="app-root"><div/>
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
 })
-export class AppComponent {
-  title = 'angular-complete';
-}
+export class AppComponent {}
