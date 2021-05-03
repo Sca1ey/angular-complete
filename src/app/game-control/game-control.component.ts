bd22904c0bb6bd0364870299807aa68ef1a307a5
Sca1ey <tim@thurgoland.net>
2021-05-03 09:01:31 +0100

    add game-control layout and logic

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css'],
+})
+export class GameControlComponent {
+  @Output() interval;
+  @Output() value: number = 0;
+
+  start() {
+    this.interval = setInterval(() => {
+      this.value++;
+      console.log(this.value);
+    }, 2000);
+    console.log('Start');
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    this.value = 0;
+    console.log('Stop');
+  }
+}
