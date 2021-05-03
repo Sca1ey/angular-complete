75f1e01ffa43ed9777255f44b146334d07bfe9c7
Sca1ey <tim@thurgoland.net>
2021-05-03 14:30:57 +0100

    add menu nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  menuItem: string = 'recipes';
+
+  ngOnInit(): void {}
+
+  onMenuEvent(menuItem: string) {
+    this.menuItem = menuItem;
+  }
+}
