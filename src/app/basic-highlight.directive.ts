26c34b4ddff2d8b6bd104d538f99a1ff217ec179
Sca1ey <tim@thurgoland.net>
2021-05-04 20:04:31 +0100

    basic directive

---
+++ src/app/basic-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import { element } from 'protractor';
+
+@Directive({
+  selector: '[appBasicHighlight]',
+})
+export class BasicHighlightDirective {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'blue';
+  }
+}
