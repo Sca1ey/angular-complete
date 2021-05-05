d91aea23db11a7e3d81c4320a2a776fdff022df5
Sca1ey <tim@thurgoland.net>
2021-05-05 17:19:25 +0100

    appUnless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  numbers: number[] = [1, 2, 3, 4, 5, 6];
+}
